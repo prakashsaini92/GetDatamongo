bd817e42978f1e4d74a898e1fb048675b44353ba
Prakash Saini <Prakash@gisdc.doit>
2019-08-05 18:21:39 +0530

    ok

---
+++ precache-manifest.e949c89906ef9a2e80dce7bba55cc7ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8704034ec744e4ee6a961e2c5a52ece",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3586c3557b97508fa23c",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5e33d534ea9da3feef65",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3586c3557b97508fa23c",
+    "url": "./static/js/2.3550e4c2.chunk.js"
+  },
+  {
+    "revision": "5e33d534ea9da3feef65",
+    "url": "./static/js/main.d6064d3b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
